cc3093b7659ffe95cf7774ad0cf677cf85f37249
Simon Bartlett <simon.bartlett@me.com>
2013-01-28 23:53:22 -0500

    Simplified Coordinate's parse methods, and added support for coordinates wrapped with brackets

--- Geo.Tests/Geo/CoordinateTests.cs
+++ Geo.Tests/Geo/CoordinateTests.cs
@@ -11,9 +11,10 @@ namespace Geo.Tests.Geo
         [TestCase("12.345N 123.456E", 12.345, 123.456)]
         [TestCase("12°N 34°W", 12, -34)]
         [TestCase("42.294498, -89.637901", 42.294498, -89.637901)]
+        [TestCase("(42.294498, -89.637901)", 42.294498, -89.637901)]
+        [TestCase("[42.294498, -89.637901]", 42.294498, -89.637901)]
+        [TestCase(" ( 42.294498, -89.637901 ) ", 42.294498, -89.637901)]
         [TestCase("42° 17′ 40″ N, 89° 38′ 16″ W", 42.294444444444444d, -89.637777777777785d)]
-        [TestCase("5107212N 00149174W", 51.1202, -1.8195666666666668d)]
-        [TestCase("5107212N00149174W", 51.1202, -1.8195666666666668d)]
         public void Parse(string coordinate, double latitude, double longitude)
         {
             var result = Coordinate.Parse(coordinate);
