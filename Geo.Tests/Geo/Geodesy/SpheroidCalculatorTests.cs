af24f1a04928f8d0404b570f0af6816b7079661e
Simon Bartlett <simon.bartlett@me.com>
2013-03-12 18:00:35 -0400

    Using a tolerance in SpheroidCalculatorTests

--- Geo.Tests/Geo/Geodesy/SpheroidCalculatorTests.cs
+++ Geo.Tests/Geo/Geodesy/SpheroidCalculatorTests.cs
@@ -8,56 +8,58 @@ namespace Geo.Tests.Geo.Geodesy
     [TestFixture]
     public class SpheroidCalculatorTests
     {
-        [TestCase(25, 1543.0305675812808d)]
-        [TestCase(-25, -1543.0305675812808d)]
+		private const double Millionth = 0.000001;
+
+        [TestCase(25, 1543.030567)]
+        [TestCase(-25, -1543.030567)]
         public void MeridionalParts(double latitude, double parts)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateMeridionalParts(latitude);
-            Assert.That(result, Is.EqualTo(parts));
+            Assert.That(result, Is.EqualTo(parts).Within(Millionth));
         }
 
-        [TestCase(25, 1493.5497673574687d)]
+        [TestCase(25, 1493.549767)]
         public void MeridionalDistance(double latitude, double parts)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateMeridionalDistance(latitude);
-            Assert.That(result.ConvertTo(DistanceUnit.Nm), Is.EqualTo(parts));
+            Assert.That(result.ConvertTo(DistanceUnit.Nm), Is.EqualTo(parts).Within(Millionth));
         }
 
-        [TestCase(0, 0, 10, 10, 845.10005801566729d)]
+        [TestCase(0, 0, 10, 10, 845.100058)]
         public void CalculateLoxodromicLineDistance(double lat1, double lon1, double lat2, double lon2, double distance)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateLoxodromicLine(new Point(lat1, lon1), new Point(lat2, lon2));
-            Assert.That(result.Distance.ConvertTo(DistanceUnit.Nm).Value, Is.EqualTo(distance));
+			Assert.That(result.Distance.ConvertTo(DistanceUnit.Nm).Value, Is.EqualTo(distance).Within(Millionth));
         }
 
-        [TestCase(0, 0, 10, 10, 45.04429310980561)]
+        [TestCase(0, 0, 10, 10, 45.044293)]
         public void CalculateLoxodromicCourse(double lat1, double lon1, double lat2, double lon2, double distance)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateLoxodromicLine(new Point(lat1, lon1), new Point(lat2, lon2));
-            Assert.That(result.Bearing12, Is.EqualTo(distance));
+			Assert.That(result.Bearing12, Is.EqualTo(distance).Within(Millionth));
         }
 
-        [TestCase(0, 0, 10, 10, 44.751910170513703d, 225.62903685894344d)]
+        [TestCase(0, 0, 10, 10, 44.751910, 225.629037)]
         public void CalculateOrthodromicCourse(double lat1, double lon1, double lat2, double lon2, double c12, double c21)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateOrthodromicLine(new Point(lat1, lon1), new Point(lat2, lon2));
-            Assert.That(result.Bearing12, Is.EqualTo(c12));
-            Assert.That(result.Bearing21, Is.EqualTo(c21));
+			Assert.That(result.Bearing12, Is.EqualTo(c12).Within(Millionth));
+			Assert.That(result.Bearing21, Is.EqualTo(c21).Within(Millionth));
         }
 
-        [TestCase(0, 0, 56, 34, 0.31843626431567573, 0.46895086740918501)]
-        [TestCase(-9.443333, 147.216667, 327.91252158264797, 50, -8.7337170355022362, 146.76964429447122d)]
+        [TestCase(0, 0, 56, 34, 0.318436, 0.468951)]
+        [TestCase(-9.443333, 147.216667, 327.912522, 50, -8.733717, 146.769644)]
         public void CalculateOrthodromicDestination(double lat1, double lon1, double angle, double distance, double lat2, double lon2)
         {
             var calculator = SpheroidCalculator.Wgs84();
             var result = calculator.CalculateOrthodromicLine(new Point(lat1, lon1), angle, new Distance(distance, DistanceUnit.Nm).SiValue);
-            Assert.That(result.Coordinate2.Latitude, Is.EqualTo(lat2));
-            Assert.That(result.Coordinate2.Longitude, Is.EqualTo(lon2));
+			Assert.That(result.Coordinate2.Latitude, Is.EqualTo(lat2).Within(Millionth));
+			Assert.That(result.Coordinate2.Longitude, Is.EqualTo(lon2).Within(Millionth));
         }
     }
 }
