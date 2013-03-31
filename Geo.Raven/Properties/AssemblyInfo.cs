5bc5735c01a5d768f8efa98cc5caabdacad7281a
Simon Bartlett <simon.bartlett@me.com>
2013-03-30 23:07:56 -0400

    Tidied up AssemblyInfo files

--- Geo.Raven/Properties/AssemblyInfo.cs
+++ Geo.Raven/Properties/AssemblyInfo.cs
@@ -5,13 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Geo.Raven")]
-[assembly: AssemblyDescription("RavenDB Integration for Geo")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Geo.Raven")]
-[assembly: AssemblyCopyright("Copyright © Simon Bartlett 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("RavenDB Integration for Geo")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44f6700e-053e-493c-bd36-5b9c842cf326")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,25 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Geo.Raven.Silverlight5")]
-[assembly: AssemblyDescription("RavenDB Integration for Geo")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Geo.Raven.Silverlight5")]
 [assembly: AssemblyCopyright("Copyright © Simon Bartlett 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a3b76d9c-e0ef-4b8f-8477-00f406c11a3d")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
