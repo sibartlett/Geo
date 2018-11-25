88ea162cb0887813bef26fd2dbc433aac223f001
Max Feingold <mfeingol@outlook.com>
2018-11-24 17:53:13 -0800

    Geo: replace PCL with .NET Standard 1.4 library

--- Geo/Properties/AssemblyInfo.cs
+++ Geo/Properties/AssemblyInfo.cs
@@ -3,6 +3,4 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Geo")]
-[assembly: AssemblyProduct("Geo")]
 [assembly: AssemblyDescription("A simple geospatial library for .NET")]
