4206b42a3e551976aa545a5a30750f12e8f2dc6c
Max Feingold <mfeingol@outlook.com>
2018-11-24 16:50:50 -0800

    Geo: preserve track point times

--- Geo/Gps/Serialization/Gpx10Serializer.cs
+++ Geo/Gps/Serialization/Gpx10Serializer.cs
@@ -95,7 +95,9 @@ namespace Geo.Gps.Serialization
                             lat = (decimal)segment.Fixes[j].Coordinate.Latitude,
                             lon = (decimal)segment.Fixes[j].Coordinate.Longitude,
                             ele = segment.Fixes[j].Coordinate.Is3D ? (decimal) ((Is3D)segment.Fixes[j].Coordinate).Elevation : 0m,
-                            eleSpecified = segment.Fixes[j].Coordinate.Is3D
+                            eleSpecified = segment.Fixes[j].Coordinate.Is3D,
+                            time = segment.Fixes[j].TimeUtc,
+                            timeSpecified = segment.Fixes[j].TimeUtc != DateTime.MinValue
                         };
                     }
                     trk.trkseg[i] = new GpxTrackSegment { trkpt = pts };
