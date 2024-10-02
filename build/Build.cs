c7a4425823712afa06ed3e72aa412f6f8fe65ce5
Simon Bartlett <simon@sibartlett.com>
2024-10-02 00:18:05 -0400

    Add nuke and GitHub CI pipeline

---
+++ build/Build.cs
@@ -0,0 +1,79 @@
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+
+[GitHubActions(
+    "ci",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(Test) }
+)]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
+
+    Target Clean =>
+        _ =>
+            _.Before(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetClean(_ => _.SetProject(Solution).SetConfiguration(Configuration));
+                });
+
+    Target Restore =>
+        _ =>
+            _.Executes(() =>
+            {
+                DotNetTasks.DotNetRestore(_ => _.SetProjectFile(Solution));
+            });
+
+    Target Compile =>
+        _ =>
+            _.DependsOn(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetBuild(
+                        _ =>
+                            _.SetProjectFile(Solution)
+                                .SetNoRestore(InvokedTargets.Contains(Restore))
+                                .SetConfiguration(Configuration)
+                    );
+                });
+
+    Target Test =>
+        _ =>
+            _.DependsOn(Compile)
+                .Executes(() =>
+                {
+                    var projects = Solution.GetAllProjects("*.Tests");
+                    foreach (var project in projects)
+                    {
+                        DotNetTasks.DotNetTest(
+                            _ => _.SetProjectFile(project.Path).SetConfiguration(Configuration).EnableNoBuild()
+                        );
+                    }
+                });
+
+    Target Publish =>
+        _ =>
+            _.Executes(() =>
+            {
+                DotNetTasks.DotNetPublish(
+                    _ => _.SetConfiguration("Release")
+                );
+            });
+
+}
